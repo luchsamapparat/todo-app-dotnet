abefefac21e2c23f4fd1374feb41f19caafefe67
Marvin Luchs <marvin@luchs.org>
2021-11-07 19:50:55 +0100

    refactor: change namespace to SsrTodo

--- Pages/Tasks/Completed.cshtml.cs
+++ Pages/Tasks/Completed.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace todo_app.Pages
+namespace SsrTodo.Pages
 {
     public class CompletedTasksModel : PageModel
     {
