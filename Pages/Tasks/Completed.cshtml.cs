3aa400a3581e643daa99c7e9378319f5508ed120
Marvin Luchs <marvin@luchs.org>
2021-11-07 19:34:13 +0100

    feat: add page templates, styles

---
+++ Pages/Tasks/Completed.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace todo_app.Pages
+{
+    public class CompletedTasksModel : PageModel
+    {
+        private readonly ILogger<CompletedTasksModel> _logger;
+
+        public CompletedTasksModel(ILogger<CompletedTasksModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
